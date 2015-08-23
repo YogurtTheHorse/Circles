3d27265635d06586be9b8c885f348db2143654df
yegorf1 <yegorf1@gmail.com>
2015-08-23 15:24:04 +0300

    Drawinf simple field

--- Circles/CircleGame.cs
+++ Circles/CircleGame.cs
@@ -5,15 +5,22 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Storage;
 using Microsoft.Xna.Framework.Input;
+using C3.XNA;
 
 #endregion
 
 namespace Circles {
     public class CircleGame : Game {
+        public static CircleGame instance;
+
         private GraphicsDeviceManager graphics;
         private SpriteBatch spriteBatch;
 
+        private Circle[,] firstPlayerField;
+
         public CircleGame() {
+            instance = this;
+
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
         }
@@ -23,6 +30,19 @@ namespace Circles {
 
             this.Window.AllowUserResizing = true;
             this.IsMouseVisible = true;
+            this.graphics.PreferMultiSampling = true;
+
+            InitFields();
+        }
+
+        public void InitFields() {
+            firstPlayerField = new Circle[Constants.FIELD_WIDTH, Constants.FIELD_HEIGHT];
+
+            for (int i = 0; i < Constants.FIELD_WIDTH; i++) {
+                for (int j = 0; j < Constants.FIELD_HEIGHT; j++) {
+                    firstPlayerField[i, j] = new Circle(i, j, Constants.FIRST_PLAYER_COLOR);
+                }
+            }
         }
 
         protected override void LoadContent() {
@@ -39,10 +59,39 @@ namespace Circles {
             base.Update(gameTime);
         }
 
+        public Vector2 GetScreenSize() {
+            return new Vector2
+            {
+                X = GetScreenWidth(),
+                Y = GetScreenHeight()
+            };
+        }
+
+        public float GetScreenWidth() {
+            return GraphicsDevice.Viewport.Width;
+        }
+
+        public float GetScreenHeight() {
+            return GraphicsDevice.Viewport.Height;
+        }
+
         protected override void Draw(GameTime gameTime) {
-            graphics.GraphicsDevice.Clear(Color.CornflowerBlue);
-            
+            graphics.GraphicsDevice.Clear(Color.White);
+
             base.Draw(gameTime);
+            spriteBatch.Begin();
+            DrawField();
+            spriteBatch.End();
+        }
+
+        protected void DrawField() {
+            float stepX = 1f / Constants.FIELD_WIDTH, stepY = 1f / Constants.FIELD_HEIGHT;
+
+            for (int i = 0; i < Constants.FIELD_WIDTH; i++) {
+                for (int j = 0; j < Constants.FIELD_HEIGHT; j++) {
+                    firstPlayerField[i, j].Draw(spriteBatch);
+                }
+            }
         }
     }
 }
