8e6b78ab130df4f168371253c9a5e7b7f5d8b736
yegorf1 <yegorf1@gmail.com>
2015-08-23 20:33:38 +0300

    Changed Line to class and added constructor for Point, Draw method

--- Circles/Line.cs
+++ Circles/Line.cs
@@ -1,13 +1,24 @@
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using C3.XNA;
 
 namespace Circles {
-    public struct Line {
+    public class Line {
         public Vector2 begin;
         public Vector2 end;
 
         public Line(Vector2 begin, Vector2 end) {
             this.begin = begin;
             this.end = end;
+        }
+
+        public Line(Point begin, Point end) {
+            this.begin = new Vector2(begin.X, begin.Y);
+            this.end = new Vector2(end.X, end.Y);
+        }
+
+        public void Draw(SpriteBatch batch) {
+            Primitives2D.DrawLine(batch, begin, end, Constants.COLORS[CircleGame.CurrentTurn], 10f);
         }
     }
 }
