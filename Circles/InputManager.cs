7e60ddc71640e74393d6e5746666590797e5470d
yegorf1 <yegorf1@gmail.com>
2015-08-23 19:53:33 +0300

    Created InputManager class

---
+++ Circles/InputManager.cs
@@ -0,0 +1,15 @@
+#region Using Statements
+using System;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Storage;
+using Microsoft.Xna.Framework.Input;
+using C3.XNA;
+
+#endregion
+
+namespace Circles {
+    class InputManager {
+    }
+}
