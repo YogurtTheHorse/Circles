5d6fe5447710e799477a2b5f069b7c9c937c10dc
yegorf1 <yegorf1@gmail.com>
2015-08-23 13:43:01 +0300

    Now game is in window and resizable

--- Circles/CircleGame.cs
+++ Circles/CircleGame.cs
@@ -9,38 +9,39 @@ using Microsoft.Xna.Framework.Input;
 #endregion
 
 namespace Circles {
-	public class CircleGame : Game {
-		private GraphicsDeviceManager graphics;
-		private SpriteBatch spriteBatch;
+    public class CircleGame : Game {
+        private GraphicsDeviceManager graphics;
+        private SpriteBatch spriteBatch;
 
-		public CircleGame() {
-			graphics = new GraphicsDeviceManager(this);
-			Content.RootDirectory = "Content";	            
-			graphics.IsFullScreen = true;		
-		}
+        public CircleGame() {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
 
-		protected override void Initialize() {
-			base.Initialize();
-		}
+        protected override void Initialize() {
+            base.Initialize();
 
-		protected override void LoadContent() {
-			spriteBatch = new SpriteBatch(GraphicsDevice);
-		}
+            this.Window.AllowUserResizing = true;
+        }
 
-		protected override void Update(GameTime gameTime) {
-			#if !__IOS__
-			if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed ||
-			    Keyboard.GetState().IsKeyDown(Keys.Escape)) {
-				Exit();
-			}
-			#endif
-			base.Update(gameTime);
-		}
+        protected override void LoadContent() {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+        }
 
-		protected override void Draw(GameTime gameTime) {
-			graphics.GraphicsDevice.Clear(Color.CornflowerBlue);
+        protected override void Update(GameTime gameTime) {
+            #if !__IOS__
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed ||
+                Keyboard.GetState().IsKeyDown(Keys.Escape)) {
+                Exit();
+            }
+            #endif
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime) {
+            graphics.GraphicsDevice.Clear(Color.CornflowerBlue);
             
-			base.Draw(gameTime);
-		}
-	}
+            base.Draw(gameTime);
+        }
+    }
 }
