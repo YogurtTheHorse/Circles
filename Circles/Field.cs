18eb6855c107690a1f82ada11ca6ee448d552d57
yegorf1 <yegorf1@gmail.com>
2015-08-24 15:20:44 +0300

    Saves lines

--- Circles/Field.cs
+++ Circles/Field.cs
@@ -1,15 +1,20 @@
 using System;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using System.Collections.Generic;
 
 namespace Circles {
     public class Field {
         private Circle[,] circles;
         private int player;
 
+        private List<Line> connections;
+
         public Field(int player) {
             this.player = player;
-            circles = new Circle[Constants.FIELD_WIDTH, Constants.FIELD_HEIGHT];
+            this.circles = new Circle[Constants.FIELD_WIDTH, Constants.FIELD_HEIGHT];
+
+            this.connections = new List<Line>();
 
             for (int i = 0; i < Constants.FIELD_WIDTH; i++) {
                 for (int j = 0; j < Constants.FIELD_HEIGHT; j++) {
@@ -26,10 +31,19 @@ namespace Circles {
             Circle a = this[begin];
             Circle b = this[end];
 
-            return a.Connect(b);
+            if (a.Connect(b)) {
+                connections.Add(new Line(Circle.GetCenterPosition(a), Circle.GetCenterPosition(b)));
+                return true;
+            }
+
+            return false;
         }
 
         public void Draw(SpriteBatch spriteBatch) {
+            foreach (Line l in connections) {
+                l.Draw(spriteBatch);
+            }
+
             for (int i = 0; i < Constants.FIELD_WIDTH; i++) {
                 for (int j = 0; j < Constants.FIELD_HEIGHT; j++) {
                     circles[i, j].Draw(spriteBatch);
