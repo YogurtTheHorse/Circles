52871a393bdd12c94915966baabc74540e0c4940
yegorf1 <yegorf1@gmail.com>
2015-08-24 15:22:41 +0300

    Lines saves color

--- Circles/Line.cs
+++ Circles/Line.cs
@@ -6,19 +6,24 @@ namespace Circles {
     public class Line {
         public Vector2 begin;
         public Vector2 end;
+        public Color color;
 
         public Line(Vector2 begin, Vector2 end) {
             this.begin = begin;
             this.end = end;
+
+            this.color = Constants.COLORS[CircleGame.CurrentTurn];
         }
 
         public Line(Point begin, Point end) {
             this.begin = new Vector2(begin.X, begin.Y);
             this.end = new Vector2(end.X, end.Y);
+
+            this.color = Constants.COLORS[CircleGame.CurrentTurn];
         }
 
         public void Draw(SpriteBatch batch) {
-            Primitives2D.DrawLine(batch, begin, end, Constants.COLORS[CircleGame.CurrentTurn], 10f);
+            Primitives2D.DrawLine(batch, begin, end, color, 10f);
         }
     }
 }
