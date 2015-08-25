531f91f9bef54df3142407cdb79d8f40b078e53f
yegorf1 <yegorf1@gmail.com>
2015-08-25 19:48:07 +0300

    Created different projects for different OSes

---
+++ Circles-Windows/Program.cs
@@ -0,0 +1,20 @@
+﻿using Circles;
+using System;
+
+namespace Circles_Windows {
+#if WINDOWS || LINUX
+    /// <summary>
+    /// The main class.
+    /// </summary>
+    public static class Program {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main() {
+            using (var game = new CircleGame())
+                game.Run();
+        }
+    }
+#endif
+}
