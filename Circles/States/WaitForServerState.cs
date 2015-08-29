890e42bfbb1e179a09c70251d62556ace8b6622f
yegorf1 <yegorf1@gmail.com>
2015-08-29 22:29:15 +0300

    Forgot to include some files

---
+++ Circles/States/WaitForServerState.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Lines.Utils;
+
+namespace Lines.States {
+    public class WaitForServerState : State {
+        public WaitForServerState() {
+            LinesGame.client = new Network.LinesClient();
+            LinesGame.client.OnGameStarted += (i) => { LinesGame.CurrentState = new OpeningState(false); };
+        }
+
+        public void Draw(SpriteBatch batch) {
+            string text = "Waiting for some server...";
+            Vector2 position = Constants.ToScreen(0.5f, 0.5f) - LinesGame.Font.MeasureString(text) / 2;
+            Color color = Constants.COLORS[Constants.DRAW];
+
+            batch.DrawString(LinesGame.Font, text, position, color);
+        }
+
+        public void Update(GameTime gameTime) {
+            LinesGame.client.Update();
+        }
+    }
+}
