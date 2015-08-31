3c32f8c528c43b4089ed0cb33b32c8bbfb376c7f
yegorf1 <yegorf1@gmail.com>
2015-08-31 18:29:42 +0300

    Added server back and added time to messages

---
+++ Lines.Server/Program.cs
@@ -0,0 +1,19 @@
+﻿using Lines.Network;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lines.Server {
+    public class Program {
+        public static void Main(string[] args) {
+            LinesServer server = new LinesServer();
+            server.Start(log);
+        }
+
+        public static void log(string s) {
+            Console.WriteLine(DateTime.Now.ToString("h:mm:ss") + "# " + s);
+        }
+    }
+}
